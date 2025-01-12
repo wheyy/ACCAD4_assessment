9ad68c6a0def1661f96d234208de1489e8212b6c
ACCAD-demos <accad-demos@mail.com>
2025-01-12 15:29:37 +0800

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Welcome to Flask via CodeDeploy!"
+    return "Welcome to Flask via CodeDeploy! Testing the pipeline"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8000)
